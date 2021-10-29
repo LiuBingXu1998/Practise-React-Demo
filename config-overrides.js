faf6d5b787083b495dbc973cfd297fb3960dcbd3
LiuBingXu <lbxemail@icloud.com>
2021-10-29 19:48:24 +0800

    完成antD组件加载

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: {
+            '@primary-color': '#495057',   // 全局主色
+            '@border-radius-base': '8px',  // 组件/浮层圆角
+        },
+    }),
+);
