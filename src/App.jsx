01fc58b83db308a05d6dd15f70f963a78e70972e
LiuBingXu <lbxemail@icloud.com>
2021-10-29 22:05:56 +0800

    正式完成初始化

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                React 练习项目
+                React项目
             </div>
         )
     }
